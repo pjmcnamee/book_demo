52b96f2d3d60e99843770e9f154ae61211cf0c0a
Patrick McNamee <pjmcnamee@comcast.net>
2019-04-30 11:59:36 -0600

    Pray to the rng gods

--- server/server.js
+++ server/server.js
@@ -2,12 +2,12 @@ require("dotenv").config();
 const express = require("express");
 const massive = require("massive");
 const session = require("express-session");
-const authCtrl = require('./controllers/authCtrl')
-const accCtrl = require('./controllers/accCtrl')
-const bookCtrl = require('./controllers/bookCtrl')
-const articlesCtrl = require('./controllers/articlesCtrl')
-const newsLetterCtrl = require('./controllers/newsLetterCtrl')
-const eventsCtrl = require('./controllers/eventsCtrl')
+const authCtrl = require("./controllers/authCtrl");
+const accCtrl = require("./controllers/accCtrl");
+const bookCtrl = require("./controllers/bookCtrl");
+const articlesCtrl = require("./controllers/articlesCtrl");
+const newsLetterCtrl = require("./controllers/newsLetterCtrl");
+const eventsCtrl = require("./controllers/eventsCtrl");
 
 const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING } = process.env;
 
@@ -22,43 +22,46 @@ massive(CONNECTION_STRING).then(db => {
   );
 });
 
-app.use(session({
-  secret: SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false,
-  cookie: {
-    maxAge: 1000* 60 * 60 * 24 * 365
-  }
-}))
+app.use(
+  session({
+    secret: SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 365
+    }
+  })
+);
 
+app.use(express.static(`${__dirname}/../build`));
 
 //user Auth
-app.get('/auth/user-data', authCtrl.userData)
-app.post('/auth/register', authCtrl.register)
-app.post('/auth/login', authCtrl.login)
-app.get('/auth/logout', (req,res) => {
-  req.session.destroy()
-  res.redirect('http://localhost:3000/#/')
-})
+app.get("/auth/user-data", authCtrl.userData);
+app.post("/auth/register", authCtrl.register);
+app.post("/auth/login", authCtrl.login);
+app.get("/auth/logout", (req, res) => {
+  req.session.destroy();
+  res.redirect("http://localhost:3000/#/");
+});
 
 //user edit
-app.put('/api/update/:id', accCtrl.update)
+app.put("/api/update/:id", accCtrl.update);
 
 //book endpoints
-app.post('/api/addbook',bookCtrl.add)
-app.get('/api/books', bookCtrl.getBooks)
-app.delete('/api/books/:id', bookCtrl.deleteBook)
+app.post("/api/addbook", bookCtrl.add);
+app.get("/api/books", bookCtrl.getBooks);
+app.delete("/api/books/:id", bookCtrl.deleteBook);
 
 //admin endpoints
 
 //articles control
-app.post('/api/addArticles', articlesCtrl.addArticle)
-app.get('/api/articles', articlesCtrl.getArticles)
+app.post("/api/addArticles", articlesCtrl.addArticle);
+app.get("/api/articles", articlesCtrl.getArticles);
 
 //newsletter control
-app.post("/api/newsletter", newsLetterCtrl.subscribe)
+app.post("/api/newsletter", newsLetterCtrl.subscribe);
 
 //event endpoints
-app.get('/api/events', eventsCtrl.getEvents)
-app.post('/api/events', eventsCtrl.addEvent)
-app.delete('/api/events/:id', eventsCtrl.deleteEvent)
+app.get("/api/events", eventsCtrl.getEvents);
+app.post("/api/events", eventsCtrl.addEvent);
+app.delete("/api/events/:id", eventsCtrl.deleteEvent);
